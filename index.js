ae78bb96fce8aadd73db6f54e0f3856537cad2ed
Devanshu Saxena <devthegreat974@gmail.com>
2021-08-01 18:15:56 -0400

    Troubleshooting deployment 1

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(fileUpload())
 if(process.env.NODE_ENV === "production") {
     //serve static content
     //npm run build
-    app.use(express.static(path.join(__dirname, "client/")))
+    app.use(express.static("build"))
 }
 
 
