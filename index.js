61510aaa38b215d5dc48a9c011ddb79a78f2c61c
Devanshu Saxena <devthegreat974@gmail.com>
2021-08-01 17:59:58 -0400

    Changed client/build to client/ in app.use(express.static())

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(fileUpload())
 if(process.env.NODE_ENV === "production") {
     //serve static content
     //npm run build
-    app.use(express.static(path.join(__dirname, "client/build")))
+    app.use(express.static(path.join(__dirname, "client/")))
 }
 
 
